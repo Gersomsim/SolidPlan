6e63697e821fd4751e0540e2a226c5785be5099d
Gersomsim <gesomhernandez@me.com>
2026-03-27 22:29:32 -0600

    Public layout

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -3,11 +3,17 @@ import { Route } from '@angular/router'
 export const appRoutes: Route[] = [
 	{
 		path: '',
-		loadComponent: () => import('./pages/public/home-page/home-page').then(m => m.HomePage),
-	},
-	{
-		path: 'auth',
-		loadChildren: () => import('./pages/public/auth/auth.routes').then(m => m.authRoutes),
+		loadComponent: () => import('./shared/ui/layouts/public-layout/public-layout').then(m => m.PublicLayout),
+		children: [
+			{
+				path: '',
+				loadComponent: () => import('./pages/public/home-page/home-page').then(m => m.HomePage),
+			},
+			{
+				path: 'auth',
+				loadChildren: () => import('./pages/public/auth/auth.routes').then(m => m.authRoutes),
+			},
+		],
 	},
 	{
 		path: 'system',
---
+++ apps/frontend/src/app/shared/ui/layouts/public-layout/public-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+import { RouterLink, RouterOutlet } from '@angular/router'
+
+@Component({
+  selector: 'app-public-layout',
+  imports: [RouterOutlet, RouterLink],
+  templateUrl: './public-layout.html',
+})
+export class PublicLayout {
+  readonly currentYear = new Date().getFullYear()
+}
